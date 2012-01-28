a360aa401dc1779f4d33f36dbd98a1502559624d
Olivia Zhang <olivia_ny_zhang@hotmail.com>
2012-01-28 15:21:02 -0800

    score keeping

--- game/Game.java
+++ game/Game.java
@@ -8,6 +8,11 @@ import javax.swing.*;
 
 public class Game {
 	private static int gameTime=0;
+	private static int score;
+	
+	// just a variable to check if the obstacle is
+	//inside the snake, change this when you get ur collision methods working
+	private boolean hasCaughtInside = false;
 	
 	public static void main (String [ ] args) throws InterruptedException {
 		try{
@@ -15,7 +20,21 @@ public class Game {
 		}catch(GameOverException e){
 			
 		}
+		
 	}
+	
+	
+	
+	public int getScore(int start, int end) {
+		//actual time elapsed in the game
+		if( hasCaughtInside == true) {
+			score +=10;
+		}
+		int actual_time = gameTime*4;
+		score += actual_time*10;
+		return score;
+	}
+	
 	public static void gameLoop() throws InterruptedException{
 		while(true){
 			gameTime++;
