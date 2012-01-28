86160412b4471e13f1523f68db9c0844601f070c
Peter Zhang <peter@peter-ThinkPad-E520.(none)>
2012-01-28 00:06:18 -0800

    stub class fiiles

---
+++ obstacles/Obstacle.java
@@ -0,0 +1,17 @@
+package obstacles;
+
+public abstract class Obstacle {
+	/**
+	 * 
+	 */
+	public abstract void update();
+	/**
+	 * 
+	 */
+	public abstract  void draw();
+	/**
+	 * 
+	 */
+	public abstract boolean haveCollided();
+
+}
---
+++ obstacles/TriangleObstacle.java
@@ -0,0 +1,24 @@
+package obstacles;
+
+
+public class TriangleObstacle extends Obstacle{
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void draw() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean haveCollided() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
