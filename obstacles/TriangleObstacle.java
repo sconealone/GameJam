7b40f0ff98ae914175f6d361ad8f77ae099a58c0
Billy Huang <nothing.here.now@gmail.com>
2012-01-28 16:36:14 -0800

    Commit

--- obstacles/TriangleObstacle.java
+++ obstacles/TriangleObstacle.java
@@ -1,5 +1,6 @@
 package obstacles;
 
+import java.awt.Graphics2D;
 import java.awt.Polygon;
 
 
@@ -13,15 +14,15 @@ public class TriangleObstacle extends Obstacle{
 	}
 
 	@Override
-	public void draw() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
 	public boolean haveCollided() {
 		// TODO Auto-generated method stub
 		return false;
+	}
+
+	@Override
+	public void draw(Graphics2D g) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
