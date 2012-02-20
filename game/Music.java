8ca29f9b93350439ad881fb63a2e7e8b89f15caf
Mitch Tee <mitch.tee@gmail.com>
2012-02-20 15:00:48 -0800

    bg music now loops

--- game/Music.java
+++ game/Music.java
@@ -8,20 +8,25 @@ public class Music {
 	 private InputStream in;
 	 AudioStream as;
 	 
+	 AudioData musicData = null;
+	 ContinuousAudioDataStream loop = null;
+	 
 	 public Music(){
 		 try{
-			 in = new FileInputStream("src" + File.separatorChar + "resources" + File.separatorChar + "MainTheme.wav");
+			 in = new FileInputStream("src" + File.separatorChar + "resources" + File.separatorChar + "MainTheme.wav"); 
 			 as = new AudioStream(in);
+			 musicData = as.getData();
+			 loop = new ContinuousAudioDataStream(musicData);
 		 } catch (IOException e){
 			 e.printStackTrace();
 		 }
 	 }
 	 
 	 public void play() {
-		 AudioPlayer.player.start(as);
+		 AudioPlayer.player.start(loop);
 	 }
 	 
 	 public void stopBGM() {
-		 AudioPlayer.player.stop(as);
+		 AudioPlayer.player.stop(loop);
 	 }
 }
